51d892a8b228533b983637cea6d890019eeecea5
wicknesh <102829841+wicknesh@users.noreply.github.com>
2024-08-27 22:00:08 +0530

    Add phone number and email address validation regular expression

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 var email = document.getElementById('InputEmail');
 var emailError = document.getElementById('emailError');
+var mobError = document.getElementById('mobError');
+var mob = document.getElementById('InputMob');
 
 function validate(){
-    let regEx = /^([a-zA-Z0-9.-]+)@([a-zA-z0-9-]+).([a-z]{2,3})(.[a-z]{2,3})?$/;
-    if(regEx.test(email.value)){
+    let regEx1 = /^([a-zA-Z0-9.-]+)@([a-zA-z0-9-]+).([a-z]{2,3})(.[a-z]{2,3})?$/;
+    if(regEx1.test(email.value)){
         emailError.innerText = "Valid email address";
-        return true;
+        email.style.borderColor = 'green';
     }
     else {
         emailError.innerText = "Invalid email address";
-        return false;
+        email.style.borderColor = 'red';
+    }
+
+    let regEx2 = /^(0)?(\+)?(91)?[ \-.]?\d{3}[ \-.]?\d{3}[ \-.]?\d{4}$/;
+    console.log(mob.value);
+    if(regEx2.test(mob.value)){
+        mobError.innerText = "Valid mobile number";
+        mob.style.borderColor = 'green';
+    }
+    else{
+        mobError.innerText = "Invalid mobile number";
+        mob.style.borderColor = 'red';
     }
 };
